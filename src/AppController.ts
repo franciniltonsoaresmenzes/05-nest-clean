223e4505606bfa051e7accaee562391028105cc3
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-09-24 19:29:51 -0300

    Setup Docker Compose

--- src/AppController.ts
+++ src/AppController.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello()
   }
+
+  @Get('/hello')
+  getIndex(): string {
+    return this.appService.index()
+  }
 }

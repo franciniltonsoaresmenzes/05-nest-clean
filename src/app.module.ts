f279989cbcf9189678f81079a0e4347c6a915a91
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-09-24 20:30:18 -0300

    Criando Pipe de validação do Zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './AppController'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controller/create-account.controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}

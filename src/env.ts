6a2e8b39e39d7b1bef36efaff01e22b305016331
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-09-25 20:37:42 -0300

    Configurando autenticação JWT

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3333),
+  JWT_SECRET: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
